893fdd8443b7f93bbca68257b50a9e8c98b7f538
Nathan Kinder <nkinder@redhat.com>
2014-07-15 12:17:39 -0700

    Ignore non-security tests
    This elimintes non-security hacking tests and ignores non-security
    PEP8 errors.  In addition, a convenience script was added to check
    out some of the main OpenStack project source repositories to set
    up an environment for scanning.

---
+++ get_source.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+git clone git://git.openstack.org/openstack/cinder
+git clone git://git.openstack.org/openstack/glance
+git clone git://git.openstack.org/openstack/keystone
+git clone git://git.openstack.org/openstack/neutron
+git clone git://git.openstack.org/openstack/nova
+git clone git://git.openstack.org/openstack/swift
