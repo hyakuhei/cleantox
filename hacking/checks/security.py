b4f4c2cf75b3e2e3b8dba1354aef4cc557f5ae1a
CPE <symc-dev@ubuntu.(none)>
2014-07-15 23:15:32 -0700

    Fix for S004
    deleting duplicate methods for S004
    updating check condition to only check for unsecured mktemp

--- hacking/checks/security.py
+++ hacking/checks/security.py
@@ -53,12 +53,11 @@ def sec_chmod_perms(logical_line):
 
 @core.flake8ext
 def hacking_creating_temp_file_or_dir(logical_line):
-    """Check for use of tempfile or mktemp
+    """Check for creating temp file or dir
 
     S004
     """
-    if (('tempfile.mktemp' in logical_line) or
-           ('mktemp' in logical_line)):
+    if 'mktemp' in logical_line:
         yield (0, "S004: Creating temporary file or directory")
 
 
@@ -125,16 +124,6 @@ def hacking_service_binding_all_interfaces(logical_line, physical_line):
     if 'bind' in logical_line:
         if '0.0.0.0' in physical_line:
             yield (0, "S009: binding to all interfaces")
-
-
-@core.flake8ext
-def hacking_creating_temp_file_or_dir(logical_line):
-    """Check for use of tempfile or 
-    
-    """
-    if (('import tempfile' in logical_line) or
-           ('mktemp' in logical_line)):
-        yield (0, "S004: Creating temporary file or directory")
 
 
 @core.flake8ext
